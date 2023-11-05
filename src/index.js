5defed11685aca345e12f62e9f2a8412d3cb28fd
nikslav <nikslav7@gmail.com>
2023-11-05 12:24:32 +0200

    edited routing

--- src/index.js
+++ src/index.js
@@ -4,10 +4,11 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import MainPage from './components/MainPage';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
-    <App />
+    <MainPage />
   </BrowserRouter>
 );
 
